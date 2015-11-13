d0ccec7c61ad219c8824cdf821043c39bea11227
Alexander Bierbrauer <ghostrifle@Alexanders-MacBook-Pro.local>
2015-11-13 22:12:03 +0100

    websockets r now configurable via env variable RAILS_CABLE_IP

--- web/app/controllers/application_controller.rb
+++ web/app/controllers/application_controller.rb
@@ -2,4 +2,13 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_action :set_websocket_host
+  
+  def set_websocket_host
+    if ENV['RAILS_CABLE_IP'].blank?
+      @RAILS_CABLE_IP = "127.0.0.1"
+    else
+      @RAILS_CABLE_IP = ENV['RAILS_CABLE_IP']
+    end
+  end
 end
