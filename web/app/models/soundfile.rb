fe1696cd20e9260e3f6db35d64bb1c153c643d7d
TheBino <info@airbirds.de>
2015-11-19 21:51:36 +0000

    added SFX support

--- web/app/models/soundfile.rb
+++ web/app/models/soundfile.rb
@@ -1,3 +1,12 @@
+# Soundfile::play("sfx_lap_beep")
 class Soundfile < ActiveRecord::Base
   mount_uploader :file,SoundfileUploader
+  
+  def self.play(name)
+	snd_file = Soundfile.where(name: name).first
+	if snd_file.file && !snd_file.file.path.blank?
+		pid = spawn("aplay #{snd_file.file.path}")
+		Process.detach pid
+	end
+  end
 end
