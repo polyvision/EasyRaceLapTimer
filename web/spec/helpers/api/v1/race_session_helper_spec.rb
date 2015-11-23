3a82c458fae5f4263ecc9714628599ea2edc4acb
Alexander Bierbrauer <ghostrifle@Alexanders-MBP.office-sb.sevendevs.de>
2015-11-23 12:05:39 +0100

    added database cleaner gem

--- web/spec/helpers/api/v1/race_session_helper_spec.rb
+++ web/spec/helpers/api/v1/race_session_helper_spec.rb
@@ -11,5 +11,5 @@ require 'rails_helper'
 #   end
 # end
 RSpec.describe Api::V1::RaceSessionHelper, type: :helper do
-  pending "add some examples to (or delete) #{__FILE__}"
+  #pending "add some examples to (or delete) #{__FILE__}"
 end
--- web/spec/rails_helper.rb
+++ web/spec/rails_helper.rb
@@ -6,6 +6,8 @@ abort("The Rails environment is running in production mode!") if Rails.env.produ
 require 'spec_helper'
 require 'rspec/rails'
 require 'timecop'
+require 'database_cleaner'
+
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
@@ -50,4 +52,9 @@ RSpec.configure do |config|
   # The different available types are documented in the features, such as in
   # https://relishapp.com/rspec/rspec-rails/docs
   config.infer_spec_type_from_file_location!
+
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with(:truncation)
+  end
 end
