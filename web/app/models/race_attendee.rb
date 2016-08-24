af7a02ddc8f19e3ec78bbc4b135f5f4b09526576
Alexander Bierbrauer <abierbrauer@polyvision.org>
2016-08-24 13:06:56 +0200

    added pilot json data to monitor api

--- web/app/models/race_attendee.rb
+++ web/app/models/race_attendee.rb
@@ -1,4 +1,14 @@
 class RaceAttendee < ActiveRecord::Base
   belongs_to :race_session
   belongs_to :pilot
+
+  def as_json(x)
+    t = Hash.new
+    t[:id] = self.id
+    t[:race_session_id] = self.race_session_id
+    t[:pilot_id] = self.pilot_id
+    t[:transponder_token] = self.transponder_token
+    t[:pilot] = pilot
+    return t
+  end
 end
